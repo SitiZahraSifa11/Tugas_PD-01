026ee2bd6eb335165b891e57ad4f7fa2b00fd089
SitiZahraSifa11 <160223744+SitiZahraSifa11@users.noreply.github.com>
2024-03-07 21:53:55 +0700

    Update nilaiSks.py

--- Pertemuan 3/nilaiSks.py
+++ Pertemuan 3/nilaiSks.py
@@ -5,17 +5,35 @@
 
 
 def hitungNilai(nilai):
-    if nilai >= 85:
-        return 4.00
+    if nilai >= 90:
+        return 'A', 4.0
+    elif nilai >= 85:
+        return 'A-', 3.75
+    elif nilai >= 80:
+        return 'A/B', 3.5
+    elif nilai >= 75:
+        return 'B+', 3.25
     elif nilai >= 70:
-        return 3.00
+        return 'B', 3.0
+    elif nilai >= 65:
+        return 'B-', 2.75
+    elif nilai >= 60:
+        return 'B/C', 2.5
     elif nilai >= 55:
-        return 2.00
+        return 'C+', 2.25
+    elif nilai >= 50:
+        return 'C', 2.0
+    elif nilai >= 45:
+        return 'C-', 1.75
     elif nilai >= 40:
-        return 1.00
+        return 'C/D', 1.5
+    elif nilai >= 35:
+        return 'D+', 1.25
+    elif nilai >= 30:
+        return 'D', 1.0
     else:
-        return 0.00
-    
+        return 'E', 0
+
 algoritma = float(input("Masukkan nilai Algoritma: "))
 sks_algoritma = int(input("Masukkan SKS Algoritma: "))
 perancangan_objek = float(input("Masukkan nilai Perancangan Objek: "))
@@ -30,15 +48,19 @@ english = float(input("Masukkan nilai English: "))
 sks_english = int(input("Masukkan SKS English: "))
 
 total_sks = sks_algoritma + sks_perancangan_objek + sks_kalkulus + sks_etika_profesi + sks_databases + sks_english
-total_nilai = (hitungNilai(algoritma) * sks_algoritma +
-               hitungNilai(perancangan_objek) * sks_perancangan_objek +
-               hitungNilai(kalkulus) * sks_kalkulus +
-               hitungNilai(etika_profesi) * sks_etika_profesi +
-               hitungNilai(databases) * sks_databases +
-               hitungNilai(english) * sks_english)
 
-IPK = total_nilai / total_sks
+total_nilai_bobot = 0
+for nilai, sks, mata_kuliah in [(algoritma, sks_algoritma, 'Algoritma'),
+                                (perancangan_objek, sks_perancangan_objek, 'Perancangan Objek'),
+                                (kalkulus, sks_kalkulus, 'Kalkulus'),
+                                (etika_profesi, sks_etika_profesi, 'Etika Profesi'),
+                                (databases, sks_databases, 'Databases'),
+                                (english, sks_english, 'English')]:
+    nilai_huruf, bobot = hitungNilai(nilai)
+    total_nilai_bobot += bobot * sks
+    
+    print(f"Untuk Mata kuliah {mata_kuliah} dengan {sks} SKS nilai yang di peroleh  :{nilai_huruf} ")
 
-print ("Nilai anda %i"%IPK)
+IPK = total_nilai_bobot / total_sks
 
 print(f"Total IPK Anda adalah {IPK:.2f}")
