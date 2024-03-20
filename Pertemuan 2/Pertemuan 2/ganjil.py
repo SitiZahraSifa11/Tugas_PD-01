33d8427ab0a3c8a1344d7712398234829f36a247
SitiZahraSifa11 <160223744+SitiZahraSifa11@users.noreply.github.com>
2024-03-20 15:16:57 +0700

    Delete Pertemuan 2 directory

--- Pertemuan 2/Pertemuan 2/ganjil.py
+++
@@ -1,8 +0,0 @@
-bil = int(input("Masukan angka  : "))
-
-if bil % 2 == 0:
-    result = "Ini bilangan genap"
-else : 
-    result = "Ini bilangan ganjil"
-
-print(result)
--- Pertemuan 2/Pertemuan 2/kabisat.py
+++
@@ -1,7 +0,0 @@
-tahun = int(input("Masukan tahun :"))
-
-if tahun % 4 == 0 :
-    status = ("Tahun Kabisat")
-else : 
-    status = ("Bukan tahun Kabisat")
-print(status)
